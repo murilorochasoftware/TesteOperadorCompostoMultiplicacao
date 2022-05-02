5977f86044580982547e3f982131e179103aa2ed
Murilo Rocha <software@murilorocha.com.br>
2022-05-02 19:58:43 -0300

    Teste do uso do operador de atribuição de maneira composta com o operador de multiplicação em Java.

---
+++ src/TesteOperadorCompostoMultiplicacao.java
@@ -0,0 +1,9 @@
+public class TesteOperadorCompostoMultiplicacao {
+
+    public static void main(String[] args) {
+        float comissao = 50.00f;
+        comissao *= 3;
+
+        System.out.printf("Comissão: R$ %.2f", comissao);
+    }
+}
